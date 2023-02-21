8d9ec1c0fa637baf513d79454d80570dffa18970
goormany <zarbot351@yandex.ru>
2023-02-21 12:37:07 +0300

    add admin models "Subscription"

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, Category, Author, Comment
+from .models import Post, Category, Author, Comment, Subscription
 
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Author)
 admin.site.register(Comment)
+admin.site.register(Subscription)
 
